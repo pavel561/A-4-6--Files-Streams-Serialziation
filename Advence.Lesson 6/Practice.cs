e12492fc8c8e042eee400911cadd21d9711f3a68
Pavel Gornostaev <pavel561@gmail.com>
2019-01-26 11:02:51 +0300

    Добавьте файлы проекта.

---
+++ Advence.Lesson 6/Practice.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Advence.Lesson_6
+{
+    public partial class Practice
+    {
+        /// <summary>
+        /// AL6-P1/7-DirInfo. Вывести на консоль следующую информацию о каталоге “c://Program Files”:
+        /// Имя
+        /// Дата создания
+        /// </summary>
+        public static void AL6_P1_7_DirInfo()
+        {
+			var dirInfo = new DirectoryInfo("C://Program Files");
+			Console.WriteLine(dirInfo.Name);
+			Console.WriteLine(dirInfo.CreationTime);
+        }
+
+
+        /// <summary>
+        /// AL6-P2/7-FileInfo. Получить список файлов каталога и для каждого вывести значение:
+        /// Имя
+        /// Дата создания
+        /// Размер 
+        /// </summary>
+        public static void AL6_P2_7_FileInfo()
+        {
+			var dirInfo = new DirectoryInfo("D://");
+			var files = dirInfo.GetFiles("*.txt");
+			foreach (FileInfo file in files)
+			{
+				Console.WriteLine(file.Name);
+				Console.WriteLine(file.CreationTime);
+				Console.WriteLine(file.Length);
+			}
+			
+		}
+
+        /// <summary>
+        /// AL6-P3/7-CreateDir. Создать пустую директорию “c://Program Files Copy”.
+        /// </summary>
+        public static void AL6_P3_7_CreateDir()
+        {
+			var dir = Directory.CreateDirectory("D:\\Lection_6");
+			Console.WriteLine(dir.Name);
+			Console.WriteLine(dir.CreationTime);
+			//Console.WriteLine(dir.);
+		}
+
+
+        /// <summary>
+        /// AL6-P4/7-CopyFile. Скопировать первый файл из Program Files в новую папку.
+        /// </summary>
+        public static void AL6_P4_7_CopyFile()
+        {
+			var dir = new DirectoryInfo("D:\\");
+			var files = dir.GetFiles("*.txt");
+			files[0].CopyTo("D:\\Lection_6\\text" + ".txt");
+			//Console.WriteLine(dir.Name);
+			//Console.WriteLine(dir.CreationTime);
+		}
+
+        /// <summary>
+        /// AL6-P5/7-FileChat. Написать программу имитирующую чат. 
+        /// Пускай в ней будут по очереди запрашивается реплики для User 1 и User 2 (используйте цикл из 5-10 итераций).  Сохраняйте данные реплики с ником пользователя и датой в файл на диске.
+        /// </summary>
+        public static void AL6_P5_7_FileChat()
+        {
+			for (int i = 0; i < 10; i++)
+			{
+				if (i % 2 == 0) Console.Write("User 1: ");
+				else Console.Write("User 2: ");
+
+				string text = Console.ReadLine();
+				//var stream = File.OpenWrite("D://History.txt");
+				var adapter = new StreamWriter("D://History.txt", true);
+				adapter.Write(DateTime.Now.ToShortDateString());
+				adapter.Write(" ");
+				if (i % 2 == 0) adapter.Write("User 1: ");
+				else adapter.Write("User 2: ");
+				adapter.Write(text);0
+				adapter.WriteLine();
+				adapter.Close();
+				//stream.Close();
+			}
+        }
+
+        /// <summary>
+        /// AL6-P6/7-ConsoleSrlz. (Демонстрация). 
+        /// Сериализовать обьект класса Song в XML.Вывести на консоль.
+        /// Десериализовать XML из строковой переменной в объект.
+        /// </summary>
+        public static void AL6_P6_7_ConsoleSrlzn()
+        {
+            Song song = new Song()
+            {
+                Title = "Title 1",
+                Duration = 247,
+                Lyrics = "Lyrics 1"
+            };
+           
+        }
+
+        /// <summary>
+        /// AL6-P7/7-FileSrlz.
+        /// Отредактировать предыдущий пример для поддержки сериализации и десериализации в файл.
+        /// </summary>
+        public static void AL6_P7_7_FileSrlz()
+        {
+
+        }
+
+    }
+}
---
+++ Advence.Lesson 6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace Advence.Lesson_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			//SysteIOUsageExample();
+			//WriteToFileWithStream();
+			//Practice1();
+			//ReadFromFileWithStream();
+			//WriteToFileWithStream();
+			//Practice2();
+			//BufferedStreamExample();
+			//SericalizationExample();
+			//XMLDocumentExample();
+			//Lesson.XMLReaderExample();
+			//Practice.AL6_P1_7_DirInfo();
+			//Practice.AL6_P2_7_FileInfo();
+			//Practice.AL6_P3_7_CreateDir();
+			//Practice.AL6_P4_7_CopyFile();
+			//Practice3_Task1_XMLSerizlization();
+
+			Practice.AL6_P5_7_FileChat();
+
+			Console.ReadLine();
+        }         
+    }
+}
